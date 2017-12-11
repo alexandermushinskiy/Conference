875f13d996fc7d6fea515488df81b32356a8a988
alexandermushinskiy <amushinskiy@gmail.com>
2017-12-12 01:03:05 +0200

    Add model validation

--- Conf.Management.WebApi/Models/Requests/CreateRequestModel.cs
+++ Conf.Management.WebApi/Models/Requests/CreateRequestModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Conf.Management.WebApi.Models.Requests
 {
     public class CreateRequestModel
     {
+        [Required(AllowEmptyStrings = false)]
         public string Name { get; set; }
 
         public string Description { get; set; }
@@ -12,10 +14,13 @@ namespace Conf.Management.WebApi.Models.Requests
 
         public DateTime FinishDate { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
         public string OwnerName { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
         public string OwnerEmail { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
         public string Venue { get; set; }
     }
 }
